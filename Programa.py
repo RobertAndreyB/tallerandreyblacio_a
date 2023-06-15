3e307eaab9f18ac47d8e92443f6b617a35644b42
RobertAndreyB <135990050+RobertAndreyB@users.noreply.github.com>
2023-06-15 14:53:07 +0000

    Implementación de modulos

---
+++ Programa.py
@@ -0,0 +1,10 @@
+from Módulo_funciones import sumar, restar, multiplicar, dividir
+
+print(sumar(2, 3))
+print(restar(4, 2))
+print(multiplicar(20, 9))
+print(dividir(45, 5))
+
+from algebra import distancia
+
+print(distancia(2, 4, 3, 6))
